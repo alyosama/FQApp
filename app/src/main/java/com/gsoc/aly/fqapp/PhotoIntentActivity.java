8583e1fc7300e8262324ffe8d4ae0a05d1bd2cbe
Aly Osama <alyosamah@gmail.com>
2015-03-19 13:17:18 +0200

    Adding Capture and Import Image Activity

---
+++ app/src/main/java/com/gsoc/aly/fqapp/PhotoIntentActivity.java
@@ -0,0 +1,316 @@
+package com.gsoc.aly.fqapp;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+
+public class PhotoIntentActivity extends Activity {
+
+	private static final int ACTION_TAKE_PHOTO= 1;
+    private static final int ACTION_IMPORT_PHOTO= 2;
+
+	private static final String BITMAP_STORAGE_KEY = "viewbitmap";
+	private static final String IMAGEVIEW_VISIBILITY_STORAGE_KEY = "imageviewvisibility";
+	private ImageView mImageView;
+	private Bitmap mImageBitmap;
+
+	private String mCurrentPhotoPath;
+
+	private static final String JPEG_FILE_PREFIX = "IMG_";
+	private static final String JPEG_FILE_SUFFIX = ".jpg";
+
+	private AlbumStorageDirFactory mAlbumStorageDirFactory = null;
+
+	
+	/* Photo album for this application */
+	private String getAlbumName() {
+		return getString(R.string.album_name);
+	}
+
+	
+	private File getAlbumDir() {
+		File storageDir = null;
+
+		if (Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())) {
+			
+			storageDir = mAlbumStorageDirFactory.getAlbumStorageDir(getAlbumName());
+
+			if (storageDir != null) {
+				if (! storageDir.mkdirs()) {
+					if (! storageDir.exists()){
+						Log.d("CameraSample", "failed to create directory");
+						return null;
+					}
+				}
+			}
+			
+		} else {
+			Log.v(getString(R.string.app_name), "External storage is not mounted READ/WRITE.");
+		}
+		
+		return storageDir;
+	}
+
+	private File createImageFile() throws IOException {
+		// Create an image file name
+		String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+		String imageFileName = JPEG_FILE_PREFIX + timeStamp + "_";
+		File albumF = getAlbumDir();
+		File imageF = File.createTempFile(imageFileName, JPEG_FILE_SUFFIX, albumF);
+		return imageF;
+	}
+
+	private File setUpPhotoFile() throws IOException {
+		
+		File f = createImageFile();
+		mCurrentPhotoPath = f.getAbsolutePath();
+		
+		return f;
+	}
+
+	private void setPic() {
+
+		/* There isn't enough memory to open up more than a couple camera photos */
+		/* So pre-scale the target bitmap into which the file is decoded */
+
+		/* Get the size of the ImageView */
+		int targetW = mImageView.getWidth();
+		int targetH = mImageView.getHeight();
+
+		/* Get the size of the image */
+		BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+		bmOptions.inJustDecodeBounds = true;
+		BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+		int photoW = bmOptions.outWidth;
+		int photoH = bmOptions.outHeight;
+		
+		/* Figure out which way needs to be reduced less */
+		int scaleFactor = 1;
+		if ((targetW > 0) || (targetH > 0)) {
+			scaleFactor = Math.min(photoW/targetW, photoH/targetH);	
+		}
+
+		/* Set bitmap options to scale the image decode target */
+		bmOptions.inJustDecodeBounds = false;
+		bmOptions.inSampleSize = scaleFactor;
+		bmOptions.inPurgeable = true;
+
+		/* Decode the JPEG file into a Bitmap */
+		Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+		
+		/* Associate the Bitmap to the ImageView */
+		mImageView.setImageBitmap(bitmap);
+		mImageView.setVisibility(View.VISIBLE);
+	}
+
+	private void galleryAddPic() {
+		    Intent mediaScanIntent = new Intent("android.intent.action.MEDIA_SCANNER_SCAN_FILE");
+			File f = new File(mCurrentPhotoPath);
+		    Uri contentUri = Uri.fromFile(f);
+		    mediaScanIntent.setData(contentUri);
+		    this.sendBroadcast(mediaScanIntent);
+	}
+
+	private void dispatchSelectImageIntent(int actionCode) {
+
+
+		switch(actionCode) {
+            case ACTION_TAKE_PHOTO:
+                    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    File f = null;
+                    try {
+                        f = setUpPhotoFile();
+                        mCurrentPhotoPath = f.getAbsolutePath();
+                        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(f));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        f = null;
+                        mCurrentPhotoPath = null;
+                    }
+                    startActivityForResult(takePictureIntent, actionCode);
+                    break;
+            case ACTION_IMPORT_PHOTO:
+                Intent importImageIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(importImageIntent, actionCode);
+                break;
+            default:
+                    break;
+            } // switch
+
+	}
+
+
+
+	private void handleCameraPhoto() {
+
+		if (mCurrentPhotoPath != null) {
+			setPic();
+			galleryAddPic();
+			mCurrentPhotoPath = null;
+		}
+
+	}
+    private void handleImportImage(Intent data) {
+            Uri selectedImage = data.getData();
+            String[] filePath = {MediaStore.Images.Media.DATA};
+            Cursor c = getContentResolver().query(selectedImage, filePath, null, null, null);
+            c.moveToFirst();
+            int columnIndex = c.getColumnIndex(filePath[0]);
+            mCurrentPhotoPath = c.getString(columnIndex);
+            c.close();
+            setPic();
+            mCurrentPhotoPath = null;
+    }
+
+	Button.OnClickListener mTakePicOnClickListener = 
+		new Button.OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			selectImage();
+		}
+	};
+
+
+
+    private void selectImage(){
+        final CharSequence[] options = {
+                "Capture Image", "Import Image", "Cancel"
+        };
+        AlertDialog.Builder builder = new AlertDialog.Builder(PhotoIntentActivity.this);
+        builder.setTitle("Add Image!");
+        builder.setItems(options, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+                if (options[item].equals("Capture Image")) {
+                    dispatchSelectImageIntent(ACTION_TAKE_PHOTO);
+                } else if (options[item].equals("Import Image")) {
+                    dispatchSelectImageIntent(ACTION_IMPORT_PHOTO);
+                } else if (options[item].equals("Cancel")) {
+                    dialog.dismiss();
+                }
+            }
+        });
+        builder.show();
+    }
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.photolayout);
+
+		mImageView = (ImageView) findViewById(R.id.imageView1);
+		mImageBitmap = null;
+
+		Button picBtn = (Button) findViewById(R.id.btnIntend);
+		setBtnListenerOrDisable( 
+				picBtn, 
+				mTakePicOnClickListener,
+				MediaStore.ACTION_IMAGE_CAPTURE
+		);
+
+		
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.FROYO) {
+			mAlbumStorageDirFactory = new FroyoAlbumDirFactory();
+		} else {
+			mAlbumStorageDirFactory = new BaseAlbumDirFactory();
+		}
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		switch (requestCode) {
+            case ACTION_TAKE_PHOTO: {
+                if (resultCode == RESULT_OK) {
+                    handleCameraPhoto();
+                }
+                break;
+            }// ACTION_TAKE_PHOTO
+            case ACTION_IMPORT_PHOTO: {
+                if (resultCode == RESULT_OK) {
+                    handleImportImage(data);
+                }
+                break;
+            }// ACTION_TAKE_PHOTO
+		} // switch
+	}
+
+	// Some lifecycle callbacks so that the image can survive orientation change
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		outState.putParcelable(BITMAP_STORAGE_KEY, mImageBitmap);
+		outState.putBoolean(IMAGEVIEW_VISIBILITY_STORAGE_KEY, (mImageBitmap != null) );
+		super.onSaveInstanceState(outState);
+	}
+
+	@Override
+	protected void onRestoreInstanceState(Bundle savedInstanceState) {
+		super.onRestoreInstanceState(savedInstanceState);
+		mImageBitmap = savedInstanceState.getParcelable(BITMAP_STORAGE_KEY);
+		mImageView.setImageBitmap(mImageBitmap);
+		mImageView.setVisibility(
+				savedInstanceState.getBoolean(IMAGEVIEW_VISIBILITY_STORAGE_KEY) ? 
+						ImageView.VISIBLE : ImageView.INVISIBLE
+		);
+	}
+
+	/**
+	 * Indicates whether the specified action can be used as an intent. This
+	 * method queries the package manager for installed packages that can
+	 * respond to an intent with the specified action. If no suitable package is
+	 * found, this method returns false.
+	 * http://android-developers.blogspot.com/2009/01/can-i-use-this-intent.html
+	 *
+	 * @param context The application's environment.
+	 * @param action The Intent action to check for availability.
+	 *
+	 * @return True if an Intent with the specified action can be sent and
+	 *         responded to, false otherwise.
+	 */
+	public static boolean isIntentAvailable(Context context, String action) {
+		final PackageManager packageManager = context.getPackageManager();
+		final Intent intent = new Intent(action);
+		List<ResolveInfo> list =
+			packageManager.queryIntentActivities(intent,
+					PackageManager.MATCH_DEFAULT_ONLY);
+		return list.size() > 0;
+	}
+
+	private void setBtnListenerOrDisable( 
+			Button btn, 
+			Button.OnClickListener onClickListener,
+			String intentName
+	) {
+		if (isIntentAvailable(this, intentName)) {
+			btn.setOnClickListener(onClickListener);        	
+		} else {
+			btn.setText( 
+				getText(R.string.cannot).toString() + " " + btn.getText());
+			btn.setClickable(false);
+		}
+	}
+
+}
